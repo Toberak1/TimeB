daf9cf5e9c05e312fbbc224643c6eed539277064
Toberak1 <61362061+Toberak1@users.noreply.github.com>
2020-02-26 18:32:29 -0300

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,1 +1,37 @@
+/**
+	* @module config/bot
+	* @desc Before editing, this file should be cloned from bot.js.template to bot.js
+	* @author WizardCM <bots@wizardcm.com>
+*/
 
+/**
+	@type {Object.<string, string>}
+*/
+module.exports = {
+	/**
+	 * @desc Bot username without discriminator
+	 * @default "MyTimeBot"
+	 * @type string
+	 */
+	name: "MyTimeBot",
+	/**
+	 * @desc Global bot command prefix
+	 * @default "?"
+	 * @type string
+	*/
+	prefix: "?",
+	/** 
+	 * @desc Discord API bot token (keep it secret)
+	 * @default "NjgwODU5OTQyOTU3NDgyMDMz.Xlbh9Q.uPbsWaQvr2F4scVIkDHhvWEMoqI
+"
+	 * @type string
+	 */
+	token: "NjgwODU5OTQyOTU3NDgyMDMz.Xlbh9Q.uPbsWaQvr2F4scVIkDHhvWEMoqI
+",
+	/** 
+	 * @desc Global bot response heading
+	 * @default "\uD83D\uDD52 Discord Time Bot"
+	 * @type string
+	 */
+	title: "\uD83D\uDD52 Discord Time Bot"
+};
